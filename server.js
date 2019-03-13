960315d5ba0ae039dbad9e09bdfd5fbe999a3484
xiuw <sylvi.xw@gmail.com>
2019-03-12 22:22:29 -0400

    commit

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.post('/submitQuote',(req,res) =>{
 })
 
 
-app.listen(3000,()=>{
-	console.log('Running on Port 3000');
+app.listen(process.env.Port || 3000,()=>{
+	console.log(`Running on Port ${process.env.port}`);
 })
 
