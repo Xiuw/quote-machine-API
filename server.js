f2bac1232522d9471eee93a35510bee20f7913e3
xiuw <xiuneh@Xius-Air.home>
2019-07-31 14:08:23 -0400

    updates

--- server.js
+++ server.js
@@ -6,22 +6,38 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json())
-const db = knex({
-	client:'pg',
-	connection:{
-		connectionString:process.env.DATABASE_URL,
-		ssl:true,
-	}
-})
+// const db = knex({
+// 	client:'pg',
+// 	connection:{
+// 		connectionString:process.env.DATABASE_URL,
+// 		ssl:true,
+// 	}
+// })
 
+const db=knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : '',
+    password : '',
+    database : 'db'
+  }
+});
+
+console.log("yeah");
 app.get('/',(req,res)=>{
-	res.send('It worked');
+	res.send("Server is running");
 })
 
 app.get('/getQuote',(req,res)=>{
 	db.select('*').from('quote')
-	.then(data => {
-	res.send(data);
+	.then(data => {	
+	let random = Math.floor(Math.random() * data.length);
+	if(data[random].quote===""){
+		let random = Math.floor(Math.random() * data.length);
+	}else{
+		res.json(data[random]);
+	}
 	})
 })
 
@@ -33,6 +49,7 @@ app.post('/submitQuote',(req,res) =>{
 		author:req.body.author
 	})
 	.then(response=>{
+		console.log(response);
 		res.json(response);
 	})
 	.catch(err=>console.log)
